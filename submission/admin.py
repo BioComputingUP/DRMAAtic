b12149f9d3f781611e7d815d9ab193def173f892
Damiano <damiano.clementel@gmail.com>
2021-10-20 10:09:39 +0200

    Updated Script and Parameter model

--- submission/admin.py
+++ submission/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Parameter, Script
 
 # Register your models here.
+admin.site.register(Parameter)
+admin.site.register(Script)
--- submission/models.py
+++ submission/models.py
@@ -6,10 +6,23 @@ from django.db import models
 
 # Define parameter in script
 class Parameter(models.Model):
+
+    class Type(models.Choices):
+        ANY = 'any'
+        INTEGER = 'int'
+        FLOAT = 'float'
+        STRING = 'string'
+        BOOL = 'bool'
+        FILE = 'file'
+
+    # # Define parameter name
+    # name = models.
     # Define substitution flag
-    flag = models.CharField(blank=True, default='')
+    flag = models.CharField(max_length=100, blank=True, default='')
     # Define parameter type
-    type = models.Choices(blank=True, default='any')  # TODO Implement choices
+    type = models.CharField(max_length=100, choices=Type.choices, blank=True, default=Type.ANY)
+    # Define default value
+    default = models.CharField(max_length=1000)
     # Define parameter description
     description = models.TextField(blank=False, default='')
 
@@ -18,5 +31,5 @@ class Parameter(models.Model):
 class Script(models.Model):
     # Define script name
     name = models.CharField(max_length=100, blank=False)
-    # Define script parameters
-    params = fields.DictField(child=Parameter)
+    # # Define script parameters
+    # params = fields.DictField(child=Parameter)
