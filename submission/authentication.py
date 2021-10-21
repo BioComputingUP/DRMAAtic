a7b0190b5e8b0ca933fb4bde89626c209503d7d6
damiano <damiano.clementel@gmail.com>
2021-10-21 14:22:54 +0200

    Authentication refactoring

--- submission/authentication.py
+++ submission/authentication.py
@@ -1,9 +1,11 @@
 # Import default user model
 from django.contrib.auth.models import User
 # Import token authentication
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, get_authorization_header
 # Import model for token
 from rest_framework.authtoken.models import Token
+# Define translation utils
+from django.utils.translation import gettext_lazy as _
 # Import response
 from rest_framework.response import Response
 # Import status codes
@@ -26,76 +28,85 @@ class BearerAuthentication(TokenAuthentication):
 # Extend Bearer token authentication to exchange it with an external service
 class RemoteAuthentication(BearerAuthentication):
 
-    # # Define URL to remote service
-    # # url = r'https://orcid.org/v3.0/{0:s}/record'  # Production
-    # url = r'https://pub.sandbox.orcid.org/v3.0/{0:s}/record'  # Development
+    # Define URL to remote service
+    # url = r'https://orcid.org/v3.0/{0:s}/record'  # Production
+    url = r'https://pub.sandbox.orcid.org/v3.0/{0:s}/record'  # Development
 
-    # # Define headers
-    # headers = {
-    #     'Content-Type': 'application/json',
-    #     'Bearer': '',  # Must be set on the fly
-    # }
+    # Define headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Bearer': '',  # Must be set on the fly
+    }
+
+    # Define request parameters
+    request = {
+        # Authentication request timeout
+        'timeout': 100
+    }
 
     # Override authenticate method
-    def authenticate(self, request):
+    def authenticate(self, *args, **kwargs):
         """ Remote authentication
         
         This authentication exchanges an external access token with an internal one.
         Both ORCID ID and access token must be issued. Then, access token will be used
         to authenticate given orcid ID.
         """
-        # Call parent authenticate method
-        return super().authenticate(request)
-        # # Try to execute
-        # try:
-        #     # Retrieve ORCID ID
-        #     orcid_id = request.query_params.get('orcid-id', '')
-        #     # Retrieve access token
-        #     access_token = '' request.auth
-        #     # Check that both ORCID ID is valid
-        #     valid_id = bool(re.match('^([0-9a-z]{4}-){3}([0-9a-z]{4})$', orcid_id, re.IGNORECASE))
-        #     # Check that token is valid
-        #     valid_token = isinstance(access_token, Token)
-        #     # Initialize response
-        #     response = None
-        #     # Case both id and access token are valid
-        #     if ( valid_id and valid_token ):
-        #         # Format url
-        #         url = self.url.format(orcid_id)
-        #         # Define header
-        #         header = { **self.header, self.keyword: access_token.key }
-        #         # Make a request against authorization URL
-        #         response = requests.get(self.url, timeout=400)
-        #         # Retrieve status code
-        #         status_code = response.status_code
-        #         # Case response return 200 OK
-        #         if status.is_success(status_code):
-        #             # Cast response to JSON format
-        #             data = response.json()
-        #             # Cast response to Response format
-        #             response = Response(data, status=status.HTTP_200_OK)
-        #         # Otherwise
-        #         else:
-        #             # Unset response
-        #             response = None
-        #     # Case response is set (and is valid!)
-        #     if response is not None:
-        #         # Get orcid object
-        #         orcid_dict = response.data.get('orcid-identifier', {})
-        #         # Retrieve identifier from response
-        #         retrieved_id = orcid_dict.get('path', '')
-        #         # Case retrieved id is valid
-        #         if ( retrieved_id == orcid_id ):
-        #             # Retrun user
-        #             user = User.objects.get(username=orcid_id)
-        #             # Return user
-        #             return (user, None)
-        # # Catch user not exist
-        # except User.DoesNotExist:
-        #     # Just raise authentication error
-        #     raise exceptions.AuthenticationFailed('No such user')
-        # # Otherwise, just raise error
-        # except:
-        #     # Just raise cateched error
-        #     raise
+        # Initialize user and token
+        user, token = None, None
+        # Authenticate user
+        user = self.authenticate_user(request)
+        # Authenticate token
+        token = self.authenticate_token(request, user)
+        # Return both user and token
+        return user, token
+
+    # Authenticate user
+    def authenticate_user(self, request: any) -> User:
+        # Retrieve orcid ID
+        orcid_id = request.query_params.get('orcid-id', '')
+        # Check that both orcid ID is valid
+        valid_id = bool(re.match('^([0-9a-z]{4}-){3}([0-9a-z]{4})$', orcid_id, re.IGNORECASE))
+        # Case orcid ID is not valid
+        if not valid_id:
+            # Raise authentication error
+            raise exceptions.AuthenticationFailed(_('Issued ORCID ID is not valid'))
+        # Define current User
+        user = User.objects.get(username=orcid_id)
+        # Case user is not available
+        if user is None:
+            # Raise authentication error
+            raise exceptions.AuthenticationFailed(_('No user is associated with given ORCID ID'))
+        # TODO Check that user is active
+        # Return user
+        return user
+        
+    # Authenticate token
+    def authenticate_token(self, request: any, user: User) -> Token:
+        # Split authentication header (token key, value)
+        auth_header = get_authorization_header(request).split()
+        # Retrieve authentication key
+        auth_key = auth_header[0] if len(auth_header) > 0 else None
+        # Retrieve authentication token
+        auth_token = auth_header[1].decode() if len(auth_header) > 1 else None
+        # Check authentication header
+        if ((auth_key != self.keyword.lower().encode()) or (auth_token is None)):
+            # Raise error
+            raise exceptions.AuthenticationFailed(_('Issued authentication header is not valid'))
+        # Define authentication endpoint (user username a s orcid ID)
+        auth_url = self.url.format(user.username)
+        # Define authentication header
+        auth_header = { **self.header, self.keyword: auth_token }
+        # Make a request against authorization URL
+        response = requests.get(auth_url, auth_header **self.request)
+        # Case response return 200 OK
+        if not status.is_success(response.status_code):
+            # Raise error
+            raise exceptions.AuthenticationFailed(_('Issued token is not valid'))
+        # Define token model
+        Token = self.get_model()
+        # Create token from user
+        token = Token.objects.get_or_create(user=user)
+        # Return user's token
+        return token
 
--- submission/views.py
+++ submission/views.py
@@ -15,17 +15,14 @@ from rest_framework import viewsets
 # Define token view
 class TokenView(viewsets.ViewSet):
     
-    # Define authentication class
-    authentication_classes = [RemoteAuthentication]
+    # # Define authentication class
+    # authentication_classes = [RemoteAuthentication]
 
     # Retrieve internal authorization token
-    def retrieve(self, request, pk):
+    def retrieve(self, request, pk=None):
         # Define current authenticated user
         user = request.user
         # Define token
         access_token = request.auth
-        # # Get/create internal access token
-        # access_token = Token.objects.get_or_create(user=user)
-        # # Return current access token
         # return Response({ 'access_token': access_token.key, 'orcid_id': user.username })
-        return Response({ 'orcid_id': user.username })
+        return Response({ 'orcid_id': user.username, 'access_token': access_token.key })
