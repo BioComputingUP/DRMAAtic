48cbd82b42275881ed7382621b563ab07132548f
Alessio Del Conte <ale.delconte96@gmail.com>
2021-12-13 10:53:30 +0100

    testing

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-print(os.environ.get("ENV_NAME"))
+print("env:", os.environ.get("ENV_NAME"))
 if os.environ.get("ENV_NAME") == 'production':
     from .production import *
 elif os.environ.get("ENV_NAME") == 'staging':
