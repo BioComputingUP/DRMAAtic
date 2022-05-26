0c13c10a63833f2fb204a191ca42a464aadb2dcc
Alessio Del Conte <ale.delconte96@gmail.com>
2022-05-26 17:19:13 +0200

    Quoting file names

--- submission/utils.py
+++ submission/utils.py
@@ -25,7 +25,8 @@ def format_task_params(passed_params):
             if passed_param.param.type == Parameter.Type.BOOL.value and passed_param.value:
                 formatted_params.append("{}".format(passed_param.param.flag))
             elif passed_param.param.type == Parameter.Type.FILE.value:
-                formatted_params.append("{} '{}'".format(passed_param.param.flag, passed_param.value))
+                filename = re.escape(passed_param.value)
+                formatted_params.append("{} '{}'".format(passed_param.param.flag, filename))
             else:
                 # If at the end of the flag there is a '=' then no space is required between flag and value
                 format_string = "{} {}"
