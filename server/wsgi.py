bdbbac24d2351b3ed782b2628717ce384fd2b6c9
Alessio Del Conte <ale.delconte96@gmail.com>
2023-04-13 12:35:43 +0200

    Update settings to use django.conf

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,24 +1,13 @@
 import os
 
-import django
-from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
 
-class WSGIEnvironment(WSGIHandler):
-
-    def __call__(self, environ, start_response):
-
-        print('DJANGO_ENV present', 'DJANGO_ENV' in environ)
-
-        if environ['DJANGO_ENV'] == 'development':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_dev')
-        elif environ['DJANGO_ENV'] == 'staging':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_staging')
-        elif environ['DJANGO_ENV'] == 'production':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_production')
-
-        django.setup()
-        return super(WSGIEnvironment, self).__call__(environ, start_response)
-
-
-application = WSGIEnvironment()
+def application(environ, start_response):
+    if environ['DJANGO_ENV'] == 'development':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_dev')
+    elif environ['DJANGO_ENV'] == 'staging':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_staging')
+    elif environ['DJANGO_ENV'] == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_production')
+    return get_wsgi_application()(environ, start_response)
