2e80ec14305fc01d89a4b3ebebc3d922a05a2ee3
Alessio Del Conte <ale.delconte96@gmail.com>
2024-01-17 15:56:19 +0100

    Add static files for app

--- drmaatic/apps.py
+++ drmaatic/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class SubmissionConfig(AppConfig):
+class DRMAAticAppConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'drmaatic'
     verbose_name = 'DRMAAtic - DRMAA enabled job scheduler with REST APIs'
