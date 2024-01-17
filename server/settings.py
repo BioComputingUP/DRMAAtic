8aaf3c654258f8a6302df906bdb5bfb2294df8cf
Alessio Del Conte <ale.delconte96@gmail.com>
2024-01-17 16:06:07 +0100

    Correct app name

--- server/settings.py
+++ server/settings.py
@@ -97,7 +97,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drmaatic.apps.SubmissionConfig',
+    'drmaatic.apps.DRMAAticAppConfig',
 ]
 
 MIDDLEWARE = [
